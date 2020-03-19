6d90a01222452a3e7fa07631ce5e38e216dbf3dc
Korhan <korhanyuzbas@gmail.com>
2020-03-19 16:55:47 +0300

    static_root set

--- movierecommender/settings.py
+++ movierecommender/settings.py
@@ -143,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 GRAPHENE = {
     'SCHEMA': 'movierecommender.graphene_schema.schema'
