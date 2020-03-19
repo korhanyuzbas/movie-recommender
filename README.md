bd1af5867173b937a7da761c6e0c6a41efafa871
Korhan <korhanyuzbas@gmail.com>
2020-03-19 22:17:24 +0300

    follow/unfollow fixes for suggestion system running README.md added

---
+++ README.md
@@ -0,0 +1,186 @@
+# Instructions
+- Live demo is available at https://movie-recommender-korhanyuzbas.herokuapp.com
+
+- Admin panel at https://movie-recommender-korhanyuzbas.herokuapp.com/admin
+
+- GraphiQL is available at https://movie-recommender-korhanyuzbas.herokuapp.com/graphql
+
+- Dataset is used to populate database (https://www.kaggle.com/rounakbanik/the-movies-dataset/data)
+
+- Suggestion runs on Celery, it'd take 1-2 minutes to complete (runs every time user follow/unfollow movie, star or genre)
+
+# TODO
+- Suggestion score multipliers hardcoded, can be added to settings or Config model
+
+- Hits db in unusual counts
+
+- GraphQL only for retrieving data, mutation needs to be added for follow and unfollow processes
+
+- More detailed documentation
+
+- More tests (only covers user app) 
+
+- Search through movies and stars (for now, can be done via Django Admin)
+# Endpoints
+
+## Users
+
+### Signup
+
+Description: Creates a new user with given data
+
+Endpoint `POST /user/register/`
+
+Payload:
+
+```
+{
+    "username": "string",
+    "password": "string",
+    "email": "string",
+    "first_name": "string",
+    "last_name": "string",
+}
+```
+
+Response: 201 and UserMe object
+
+Note: `password` and `email` are going through validators. Weak passwords or invalid emails will cause errors.
+
+### Login
+
+Description: Authenticates given user
+
+Endpoint `POST /user/login/`
+
+Payload:
+
+```
+{
+    "username": "string",
+    "password": "string"
+}
+```
+
+Response: 200 and token object
+
+Note: You can login with username or email. Just pass the input as "username", API will accept both.
+
+### Logout
+
+`Authentication token is required in this endpoint`
+
+Description: Logout the current user
+
+Endpoint `GET /user/logout/
+
+Response: 204
+
+### Auth
+
+Authentication token is required in this endpoint`
+
+Description: Returns the information about authed user (current user)
+
+Endpoint `GET /user/me/`
+
+Response: 200 and user object
+
+## Movie
+
+### Movie List
+Authentication token is required in this endpoint`
+
+Endpoint `GET /movie/`
+
+Response: 200 and list of movie objects
+
+### Movie Retrieve
+
+Authentication token is required in this endpoint`
+
+Endpoint `GET /movie/<:id>`
+
+Response: 200 and movie object
+
+### Movie Follow
+
+Authentication token is required in this endpoint`
+
+Endpoint `POST /movie/<:id>/follow/`
+
+Response: 204 
+
+### Movie Unfollow
+
+Authentication token is required in this endpoint`
+
+Endpoint `POST /movie/<:id>/unfollow/`
+
+Response: 204 
+
+## Genre
+
+### Genre List
+Authentication token is required in this endpoint`
+
+Endpoint `GET /genre/`
+
+Response: 200 and list of genre objects
+
+### Genre Retrieve
+
+Authentication token is required in this endpoint`
+
+Endpoint `GET /genre/<:id>`
+
+Response: 200 and genre object
+
+### Genre Follow
+
+Authentication token is required in this endpoint`
+
+Endpoint `POST /genre/<:id>/follow/`
+
+Response: 204 
+
+### Genre Unfollow
+
+Authentication token is required in this endpoint`
+
+Endpoint `POST /genre/<:id>/unfollow/`
+
+Response: 204 
+
+## Artist
+
+### Artist List
+Authentication token is required in this endpoint`
+
+Endpoint `GET /artist/`
+
+Response: 200 and list of movie objects
+
+### Artist Retrieve
+
+Authentication token is required in this endpoint`
+
+Endpoint `GET /artist/<:id>`
+
+Response: 200 and movie object
+
+### Artist Follow
+
+Authentication token is required in this endpoint`
+
+Endpoint `POST /artist/<:id>/follow/`
+
+Response: 204 
+
+### Artist Unfollow
+
+Authentication token is required in this endpoint`
+
+Endpoint `POST /artist/<:id>/unfollow/`
+
+Response: 204 
--- movie/views.py
+++ movie/views.py
@@ -6,8 +6,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
+from core.models import CeleryResult
 from movie.models import Movie, Genre, Artist, MovieFollow, ArtistFollow, GenreFollow
 from movie.serializers import MovieSerializer, GenreSerializer, ArtistSerializer
+from movie.tasks import process_suggest_for_user
 
 
 class MovieViewSet(ListAPIView, RetrieveAPIView, GenericViewSet):
@@ -15,21 +17,24 @@ class MovieViewSet(ListAPIView, RetrieveAPIView, GenericViewSet):
     serializer_class = MovieSerializer
     permission_classes = []
 
-    @action(methods=['post'], detail=True)
+    @action(methods=['post'], detail=True, permission_classes=[IsAuthenticated])
     def follow(self, request, pk):
         instance = Movie.objects.filter(id=pk).first()
         if instance:
             MovieFollow.objects.get_or_create(user=request.user, movie=instance)
+            task_id = process_suggest_for_user.apply_async(kwargs={'user_id': request.user.id})
+            CeleryResult.objects.create(user_id=request.user.id, task_id=task_id, status=CeleryResult.PENDING)
             return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             raise ValidationError('Movie not found')
 
-    @action(methods=['post'], detail=True)
+    @action(methods=['post'], detail=True, permission_classes=[IsAuthenticated])
     def unfollow(self, request, pk):
         instance = MovieFollow.objects.filter(user=request.user, movie_id=pk).first()
         if not instance:
             raise ValidationError('Object not found')
-
+        task_id = process_suggest_for_user.apply_async(kwargs={'user_id': request.user.id})
+        CeleryResult.objects.create(user_id=request.user.id, task_id=task_id, status=CeleryResult.PENDING)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
@@ -38,21 +43,25 @@ class GenreViewSet(ListAPIView, RetrieveAPIView, GenericViewSet):
     serializer_class = GenreSerializer
     permission_classes = [IsAuthenticated]
 
-    @action(methods=['post'], detail=True)
+    @action(methods=['post'], detail=True, permission_classes=[IsAuthenticated])
     def follow(self, request, pk):
         movie = Genre.objects.filter(id=pk).first()
         if movie:
-            GenreFollow.objects.get_or_create(user=request.user, genre=movie)
+            _, created = GenreFollow.objects.get_or_create(user=request.user, genre=movie)
+            if created:
+                task_id = process_suggest_for_user.apply_async(kwargs={'user_id': request.user.id})
+                CeleryResult.objects.create(user_id=request.user.id, task_id=task_id, status=CeleryResult.PENDING)
             return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             raise ValidationError('Movie not found')
 
-    @action(methods=['post'], detail=True)
+    @action(methods=['post'], detail=True, permission_classes=[IsAuthenticated])
     def unfollow(self, request, pk):
         instance = GenreFollow.objects.filter(user=request.user, genre_id=pk).first()
         if not instance:
             raise ValidationError('Object not found')
-
+        task_id = process_suggest_for_user.apply_async(kwargs={'user_id': request.user.id})
+        CeleryResult.objects.create(user_id=request.user.id, task_id=task_id, status=CeleryResult.PENDING)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
@@ -61,19 +70,23 @@ class ArtistViewSet(ListAPIView, RetrieveAPIView, GenericViewSet):
     serializer_class = ArtistSerializer
     permission_classes = [IsAuthenticated]
 
-    @action(methods=['post'], detail=True)
+    @action(methods=['post'], detail=True, permission_classes=[IsAuthenticated])
     def follow(self, request, pk):
         instance = Artist.objects.filter(id=pk).first()
         if instance:
-            ArtistFollow.objects.get_or_create(user=request.user, artist=instance)
+            _, created = ArtistFollow.objects.get_or_create(user=request.user, artist=instance)
+            if created:
+                task_id = process_suggest_for_user.apply_async(kwargs={'user_id': request.user.id})
+                CeleryResult.objects.create(user_id=request.user.id, task_id=task_id, status=CeleryResult.PENDING)
             return Response(status=status.HTTP_204_NO_CONTENT)
         else:
             raise ValidationError('Movie not found')
 
-    @action(methods=['post'], detail=True)
+    @action(methods=['post'], detail=True, permission_classes=[IsAuthenticated])
     def unfollow(self, request, pk):
         instance = ArtistFollow.objects.filter(user=request.user, artist_id=pk).first()
         if not instance:
             raise ValidationError('Object not found')
-
+        task_id = process_suggest_for_user.apply_async(kwargs={'user_id': request.user.id})
+        CeleryResult.objects.create(user_id=request.user.id, task_id=task_id, status=CeleryResult.PENDING)
         return Response(status=status.HTTP_204_NO_CONTENT)
