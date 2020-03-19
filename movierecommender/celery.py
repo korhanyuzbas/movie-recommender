0bfad82bba93e5ff1ac51d7269a3dcb413a9d6f1
Korhan <korhanyuzbas@gmail.com>
2020-03-19 21:56:17 +0300

    url changed for redis

--- movierecommender/celery.py
+++ movierecommender/celery.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movierecommender.settings')
 
-app = Celery('movierecommender', broker='redis://localhost')
+app = Celery('movierecommender', broker='redis://165.227.238.244')
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
